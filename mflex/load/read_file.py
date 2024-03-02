Fixing scaling post Seehafer

mflex/load/read_file.py
@@ -119,6 +119,85 @@ def read_issi_rmhd(path: str) -> Data3D:
     )
 
 
+def read_issi_rmhd_alt(path: str, L: np.float64) -> Data3D:
+    """
+    Returns dataclass Data3D extracted from Analytical_boundary_data.sav
+    provided by ISSI Team.
+    """
+    data = scipy.io.readsav(path, python_dict=True, verbose=True)
+
+    data_bz = data["b2dz"]
+    # [0:nresol_y,0:nresol_x]
+    # Y-axis size first as this corresponds to number of rows, then X-Axis size corresponding t number of columns
+    data_bx = data["b2dx"]
+    # [0:nresol_y,0:nresol_x]
+    # Y-axis size first as this corresponds to number of rows, then X-Axis size corresponding t number of columns
+    data_by = data["b2dy"]
+    # [0:nresol_y,0:nresol_x]
+    # Y-axis size first as this corresponds to number of rows, then X-Axis size corresponding t number of columns
+
+    print(data["info_unit"])
+    print(data["info_pixel"])
+    print(data["info_array"])
+
+    nresol_x = data_bz.shape[1]
+    nresol_y = data_bz.shape[0]
+
+    pixelsize_y_Mm = 192.0 * 10**-3
+    pixelsize_x_Mm = 192.0 * 10**-3
+    pixelsize_z_Mm = 64.0 * 10**-3
+
+    xmin = 0.0  # Minimum value of x in data length scale, not in Mm
+    ymin = 0.0  # Minimum value of y in data length scale, not in Mm
+    zmin = 0.0  # Minimum value of z in data length scale, not in Mm
+
+    xmax_Mm = nresol_x * pixelsize_x_Mm
+    ymax_Mm = nresol_y * pixelsize_y_Mm
+    zmax_Mm = 41.6
+
+    nresol_z = int(np.floor(zmax_Mm / pixelsize_z_Mm))
+
+    z0 = 2000.0 * 10**-3
+
+    nf_max = min(nresol_x, nresol_y)
+
+    xmax = xmax_Mm / L
+    ymax = ymax_Mm / L
+    zmax = zmax_Mm / L
+    z0 = z0 / L
+
+    pixelsize_x = pixelsize_x_Mm / L
+    pixelsize_y = pixelsize_y_Mm / L
+    pixelsize_z = pixelsize_z_Mm / L
+
+    print("xmax, ymax, zmax", xmax, ymax, zmax)
+    print("xmax_Mm, ymax_Mm, zmax_Mm", xmax_Mm, ymax_Mm, zmax_Mm)
+    print("nresol_x, nresol_y, nresol_z", nresol_x, nresol_y, nresol_z)
+    print("pixelsize_x, pixelsize_x_Mm", pixelsize_x, pixelsize_x_Mm)
+    print("pixelsize_y, pixelsize_y_Mm", pixelsize_y, pixelsize_y_Mm)
+    print("pixelsize_z, pixelsize_z_Mm", pixelsize_z, pixelsize_z_Mm)
+
+    return Data3D(
+        data_bx,
+        data_by,
+        data_bz,
+        nresol_x,
+        nresol_y,
+        nresol_z,
+        pixelsize_x,
+        pixelsize_y,
+        pixelsize_z,
+        nf_max,
+        xmin,
+        xmax,
+        ymin,
+        ymax,
+        zmin,
+        zmax,
+        z0,
+    )
+
+
 def read_issi_rmhd_zbased(path: str) -> Data3D:
     """
     Returns dataclass Data3D extracted from RMHD_boundary_data.sav
@@ -317,7 +396,7 @@ def read_issi_analytical(path: str) -> Data3D:
     )
 
 
-def read_issi_analytical_alt(path: str) -> Data3D:
+def read_issi_analytical_alt(path: str, L: np.float64) -> Data3D:
     """
     Returns dataclass Data3D extracted from Analytical_boundary_data.sav
     provided by ISSI Team.
@@ -342,39 +421,39 @@ def read_issi_analytical_alt(path: str) -> Data3D:
     nresol_x = data_bz.shape[1]
     nresol_y = data_bz.shape[0]
 
-    pixelsize_z_km = 40.0
-    pixelsize_x_km = 40.0
-    pixelsize_y_km = 40.0
+    pixelsize_z_Mm = 40.0 * 10**-3
+    pixelsize_x_Mm = 40.0 * 10**-3
+    pixelsize_y_Mm = 40.0 * 10**-3
 
     xmin = 0.0  # Minimum value of x in data length scale, not in Mm
     ymin = 0.0  # Minimum value of y in data length scale, not in Mm
     zmin = 0.0  # Minimum value of z in data length scale, not in Mm
 
-    xmax_Mm = nresol_x * pixelsize_x_km / 1000.0
-    ymax_Mm = nresol_y * pixelsize_y_km / 1000.0
-    zmax_Mm = 2.0
+    xmax_Mm = nresol_x * pixelsize_x_Mm
+    ymax_Mm = nresol_y * pixelsize_y_Mm
+    zmax_Mm = 2000.0 * 10**-3
 
-    nresol_z = int(np.floor(zmax_Mm * 1000.0 / pixelsize_z_km))
+    nresol_z = int(np.floor(zmax_Mm / pixelsize_z_Mm))
 
-    L = 1.6
+    z0 = 2000.0 * 10**-3
+
+    nf_max = min(nresol_x, nresol_y)
 
     xmax = xmax_Mm / L
     ymax = ymax_Mm / L
     zmax = zmax_Mm / L
+    z0 = z0 / L
 
-    z0 = zmax
-    pixelsize_x = pixelsize_x_km / 1000.0 / L
-    pixelsize_y = pixelsize_y_km / 1000.0 / L
-    pixelsize_z = pixelsize_z_km / 1000.0 / L
-
-    nf_max = min(nresol_x, nresol_y)
+    pixelsize_x = pixelsize_x_Mm / L
+    pixelsize_y = pixelsize_y_Mm / L
+    pixelsize_z = pixelsize_z_Mm / L
 
     print("xmax, ymax, zmax", xmax, ymax, zmax)
     print("xmax_Mm, ymax_Mm, zmax_Mm", xmax_Mm, ymax_Mm, zmax_Mm)
     print("nresol_x, nresol_y, nresol_z", nresol_x, nresol_y, nresol_z)
-    print("pixelsize_x, pixelsize_x_km", pixelsize_x, pixelsize_x_km)
-    print("pixelsize_y, pixelsize_y_km", pixelsize_y, pixelsize_y_km)
-    print("pixelsize_z, pixelsize_z_km", pixelsize_z, pixelsize_z_km)
+    print("pixelsize_x, pixelsize_x_Mm", pixelsize_x, pixelsize_x_Mm)
+    print("pixelsize_y, pixelsize_y_Mm", pixelsize_y, pixelsize_y_Mm)
+    print("pixelsize_z, pixelsize_z_Mm", pixelsize_z, pixelsize_z_Mm)
 
     return Data3D(
         data_bx,

mflex/model/field/bfield_model.py
@@ -13,6 +13,7 @@ from mflex.model.field.utility.poloidal import (
     phi_hypgeo,
     dphidz_hypgeo,
 )
+from mflex.plot.plot_magnetogram import plot_magnetogram_boundary
 
 
 def magnetic_field(
@@ -34,7 +35,6 @@ def magnetic_field(
     pixelsize_x: np.float64,
     pixelsize_y: np.float64,
     nf_max: int,
-    L: np.float64,
 ) -> np.ndarray[np.float64, np.dtype[np.float64]]:
     """
     Given the Seehafer-mirrored photospheric magnetic field data_bz,
@@ -51,6 +51,12 @@ def magnetic_field(
     # Normalised length scale in x direction for Seehafer
     length_scale_y_norm = length_scale_y / length_scale
     # Normalised length scale in y direction for Seehafer
+
+    print("length scale", length_scale)
+    print("length scale x", length_scale_x)
+    print("length scale y", length_scale_y)
+    print("length scale x norm", length_scale_x_norm)
+    print("length scale y norm", length_scale_y_norm)
 
     print("length scale", length_scale)
     print("length scale x", length_scale_x)
@@ -190,7 +196,7 @@ def magnetic_field(
     return b_arr, bz_derivs
 
 
-def magnetic_field_ISSI_analytical(
+def magnetic_field_alt(
     data_bz: np.ndarray[np.float64, np.dtype[np.float64]],
     z0: np.float64,
     deltaz: np.float64,
@@ -217,15 +223,17 @@ def magnetic_field_ISSI_analytical(
     series expansion using anm, phi and dphidz.
     """
 
+    data_bz_seehafer = mirror_magnetogram(
+        data_bz, xmin, xmax, ymin, ymax, nresol_x, nresol_y
+    )
+
     length_scale = 2.0 * L  # Normalising length scale for Seehafer
-    length_scale_x = 2.0 * xmax * L
-    # Length scale in x direction for Seehafer
-    length_scale_y = 2.0 * ymax * L
-    # Length scale in y direction for Seehafer
+
+    length_scale_x = 2.0 * nresol_x * pixelsize_x * L
+    length_scale_y = 2.0 * nresol_y * pixelsize_y * L
+
     length_scale_x_norm = length_scale_x / length_scale
-    # Normalised length scale in x direction for Seehafer
     length_scale_y_norm = length_scale_y / length_scale
-    # Normalised length scale in y direction for Seehafer
 
     print("length scale", length_scale)
     print("length scale x", length_scale_x)
@@ -238,13 +246,15 @@ def magnetic_field_ISSI_analytical(
     if not (xmax > 0.0 or ymax > 0.0 or zmax > 0.0):
         raise ValueError("Magnetogram in wrong quadrant of Seehafer mirroring")
 
+    print("xmin, xmax, ymin, ymax, zmin, zmax ", xmin, xmax, ymin, ymax, zmin, zmax)
+
     x_arr = np.arange(2.0 * nresol_x) * 2.0 * xmax / (2.0 * nresol_x - 1) - xmax
     y_arr = np.arange(2.0 * nresol_y) * 2.0 * ymax / (2.0 * nresol_y - 1) - ymax
     z_arr = np.arange(nresol_z) * (zmax - zmin) / (nresol_z - 1) + zmin
 
     ratiodzls = deltaz / length_scale
 
-    alpha = alpha / length_scale  # Normalised deltaz
+    alpha = alpha * length_scale / L  # Normalised deltaz
 
     # kx, ky arrays, coefficients for x and y in Fourier series
 
@@ -265,9 +275,6 @@ def magnetic_field_ISSI_analytical(
     p_arr = 0.5 * ratiodzls * np.sqrt(k2_arr * (1.0 - a - a * b) - alpha**2)
     q_arr = 0.5 * ratiodzls * np.sqrt(k2_arr * (1.0 - a + a * b) - alpha**2)
 
-    data_bz_seehafer = mirror_magnetogram(
-        data_bz, xmin, xmax, ymin, ymax, nresol_x, nresol_y
-    )
     anm = fft_coeff_seehafer(
         data_bz_seehafer, k2_arr, 2 * nresol_x, 2 * nresol_y, nf_max
     )